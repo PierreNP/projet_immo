d625d52de3fb581012cd40ca1db5a30acfdea7b5
jonathan justman <jojo.justman@gmail.com>
2021-11-29 15:44:26 +0100

    avatar USER seems OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'avatars/create'
   resources :categories
   resources :listings
+  
   devise_for :users,
              controllers: {
                  sessions: 'users/sessions',
                  registrations: 'users/registrations'
              }
+  resources :users
+  post 'rails/active_storage/direct_uploads', to: 'direct_uploads#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email:"caca@caca.pipi", password:"jaimelepipi")
+
+user.avatar.attach(io: File.open("app/assets/test.jpg"), filename: "test")
