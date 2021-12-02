6ddb948be373cce994319c07be69901b77f7d176
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-12-02 10:27:41 +0100

    active mailer for forgot password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,7 +8,7 @@ class PasswordsController < ApplicationController
 
     if @user.present? 
       @user.generate_password_token!
-      # UserMailer.reset_password(@user).deliver_now
+      UserMailer.reset_password(@user).deliver_now
       render json: {status: 'ok'}, status: :ok
     else
       render json: {error: ['Email address not found. Please check and try again.']}, status: :not_found
